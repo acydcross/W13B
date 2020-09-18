e1721246448f8c56f45a82c0248771c0587a508e
acydcross <acydcross@hotmail.com>
2020-09-17 20:47:15 -0600

    added a body.vue file

---
+++ src/components/w13b/body.vue
@@ -0,0 +1,22 @@
+<template>
+    <div @click="makeRed" :class="{ bodygrid: isGrid }">
+        <h3 v-for="name in names" v-bind:key="name">{{ name }}</h3>
+    </div> 
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style scoped>
+.bodygrid {
+        display: grid;
+        justify-items: center;
+        align-items: center;
+        grid-template-columns: repeat(4, 1fr);
+    }
+
+</style>
